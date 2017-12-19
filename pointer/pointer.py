d6b0bcb05f5c6d5ed74b539395ae8bdb53230d0a
K4YT3X <k4t@protonmail.com>
2017-12-19 12:49:19 -0500

    PEP8 Autoformat & bug fixes

--- pointer/pointer.py
+++ pointer/pointer.py
@@ -1,8 +1,18 @@
+"""
+Raspberry pi Pointer Driver
+Dev: djgood@github.com
+Dev: k4yt3x@github.com
+Date Created: N/A
+Last Modified: Dec 19, 2017
+"""
+
+
 class Direction(object):
 
     def __init__(self, azimuth, altitude):
         self.azimuth = azimuth
         self.altitude = altitude
+
 
 class Pointer(object):
 
@@ -12,16 +22,18 @@ class Pointer(object):
 
     def run(self):
         pass
-        # while running:
-            # direction = calculate_direction()
-            # calculate change in direction from pointing direction and desired direction
-            # if alt change > 1 degree:
-                # update servo pos
-                # pointing direction is now set to latest update
-            # if az change > 0.9 degrees:
-                # move stepper one step and update pointing direction
+        """
+        while running:
+            direction = calculate_direction()
+            calculate change in direction from pointing direction and desired direction
+            if alt change > 1 degree:
+                update servo pos
+                pointing direction is now set to latest update
+            if az change > 0.9 degrees:
+                move stepper one step and update pointing direction
+        """
 
-    def calculate_direction(self, direction): 
+    def calculate_direction(self, direction):
         # returns az, alt
         pass
 
@@ -32,7 +44,7 @@ class Pointer(object):
     def set_servo_position(self, angle):
         # code from servo.py
         pass
-            
+
 
 if __name__ == '__main__':
     pointer = Pointer()
