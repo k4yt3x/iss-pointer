94ceba823791c8c0006492a0a243b6661726c617
K4YT3X <k4t@protonmail.com>
2018-01-16 16:42:42 -0500

    motor

--- motor.py
+++ motor.py
@@ -121,7 +121,7 @@ class Stepper(object):
 
     def set_azimuth(self, azimuth):
         print(self.current_pos)
-        current_angle = self.current_pos / 2.5 * 10 / 9
+        current_angle = 0.36 * self.current_pos
         angle_to_rotate = azimuth - current_angle
         print(current_angle)
         if angle_to_rotate == 0:
